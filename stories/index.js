621bc31b844d8c764ce0f1c091cefc5227846b2f
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-02-27 00:14:08 +0800

    [add] ImageReader.vue & Icon.vue

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 
-
 import { storiesOf } from '@storybook/vue';
+
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue'
 
 import MyButton from './Button.vue';
 import MySteps from './Steps.vue';
+import ImageReader from './ImageReader.vue';
 
 storiesOf('MyButton', module)
   .add('story as a template', () => ({
@@ -31,3 +34,51 @@ storiesOf('Steps', module)
       }
     }
   }))
+
+
+storiesOf('ImageReader', module)
+  .add('ImageReader', () => ({
+    components: { ImageReader },
+    template: `
+      <image-reader
+        v-model="imageUploadUrl"
+        :image-width-height-pair="[
+          {
+            width: 300,
+            height: 60
+          }
+        ]"
+        :isvalid.sync="isValidUploadImage"
+      >
+      </image-reader>
+    `,
+    data () {
+      return {
+        imageUploadUrl: '',
+        isValidUploadImage: false
+      }
+    }
+  }))
+
+storiesOf('Icons', module)
+  .add('Icon', () => ({
+    components: { Icon },
+    template: `
+      <div>
+        <icon name="beer"></icon>
+
+        <icon name="refresh" scale="2" spin></icon>
+        <icon name="comment" flip="horizontal"></icon>
+        <icon name="code-fork" label="Forked Repository"></icon>
+
+        <icon label="No Photos">
+          <icon name="camera"></icon>
+          <icon name="ban" scale="2" class="alert"></icon>
+        </icon>
+      </div>
+    `
+  }))
+
+
+
+

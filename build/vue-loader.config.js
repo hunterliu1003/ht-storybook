c7bdd6db36e892f63daf4f678d2ca262f180f23a
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-02-27 00:10:17 +0800

    [add] less-loader

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -8,6 +8,7 @@ module.exports = {
   ],
   loaders: {
     scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
-    sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+    sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax', // <style lang="sass">
+    less: 'vue-style-loader!css-loader!less-loader'
   }
 }

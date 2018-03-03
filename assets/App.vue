ed563ed6f7ab6e8b39b76043cca171b38fbc78dc
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-03 13:29:19 +0800

    [mod] App.vue use Pug

--- assets/App.vue
+++ assets/App.vue
@@ -1,38 +1,29 @@
-<template>
-  <v-app>
-    <v-toolbar dark class="primary">
-      <v-toolbar-side-icon
+<template lang="pug">
+  v-app
+    v-toolbar(dark class="primary")
+      v-toolbar-side-icon(
         @click.native="sideNav = !sideNav"
-        class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View MeetUps
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+        class="hidden-sm-and-up"
+      )
+      v-toolbar-title
+        | DevMeetup
+      v-spacer
+      v-toolbar-items(class="hidden-xs-only")
+        v-btn(flat)
+          v-icon(left) supervisor_account
+          | View MeetUps
 
-    <v-navigation-drawer v-model="sideNav">
-      <v-list>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            View MeetUps
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    v-navigation-drawer(v-model="sideNav")
+      v-list
+        v-list-tile
+          v-list-tile-action
+            v-icon supervisor_account
+          v-list-tile-content
+            | View MeetUps
 
-    <main>
-      <div class="red--text body-2">
-        123
-      </div>
-    </main>
-  </v-app>
+    main
+      div(class="red--text body-2")
+        | 123
 
 </template>
 

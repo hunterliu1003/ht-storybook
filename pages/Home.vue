ec59aac5162225dbb09cccddac940c78a6dfbbb6
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-03 18:54:46 +0800

    [add] carouel

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,4 +1,49 @@
 <template lang="pug">
-  div
-    p The Home Page
+  v-container
+    v-layout(row wrap)
+      v-flex.text-xs-center.text-sm-right(xs12 sm6)
+        v-btn.info(large router to='/meetups') Explore Meetups
+      v-flex.text-xs-center.text-sm-left(xs12 sm6)
+        v-btn.info(large router to='/meetups/new') Organize Meetup
+
+    v-layout.mt-2(row wrap)
+      v-flex(xs12)
+        v-carousel
+          v-carousel-item(
+            v-for='(meetup, i) in meetups'
+            :src='meetup.imageUrl'
+            :key='meetup.id'
+          )
+            div.title {{ meetup.title }}
+
+    v-layout.mt-2(row wrap)
+      v-flex.text-xs-center(xs12)
+        p Join our awesome meetups!
+
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://media.timeout.com/images/103444978/630/472/image.jpg', id: 'adfadsfdasfasdfads', title: 'Meetup in New York'},
+        { imageUrl: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/europe/france/paris/eiffel-tower-paris-p.jpg?imwidth=1160', id: 'adfa213d213213sdfads', title: 'Meetup in Paris'},
+        { imageUrl: 'https://www.telegraph.co.uk/content/dam/Travel/2018/February/Akihabara-overview.jpg?imwidth=1240', id: 'adfadsf23123dfads', title: 'Meetup in Tokyo'},
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    position absolute
+    bottom: 60px
+    background-color: rgba(0, 0, 0, .5)
+    color: white
+    font-size: 2em
+    padding: 10px
+    left: 50%;
+    transform: translate(-50%, 0);
+</style>

49e505cd936e72761ac6cb0a581f8afd5a88d06e
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-04 17:49:29 +0800

    [add] bundle files

---
+++ public/6.dcb225b8b97372619ae1.js
@@ -0,0 +1,1 @@
+webpackJsonp([6],{278:function(e,n,t){var r=t(45)(null,t(291),null,null,null);e.exports=r.exports},291:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},staticRenderFns:[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("p",[e._v("The Profile Page")])])}]}}});

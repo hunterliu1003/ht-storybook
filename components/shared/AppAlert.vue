4770cf50c94ae156a08590fb9aa07c5dcbdf656b
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-04 15:33:37 +0800

    [add] shared component AppAlert.vue

---
+++ components/shared/AppAlert.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  v-alert(error dismissible @input="onClose" :value="true")
+    | {{ text }}
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  methods: {
+      onClose () {
+          this.$emit('dismissed')
+      }
+  }
+}
+</script>

7d65ca8f4c266196cc8c1fe36a6139a9d878d6ee
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-15 17:05:30 +0800

    [add] addon-options ht-storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,21 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
 
 import Vue from 'vue';
-import Vuex from 'vuex'; // Vue plugins
+import Vuex from 'vuex';
 
-// Import your custom components.
 import Mybutton from '../stories/Button.vue';
 
-// Install Vue plugins.
 Vue.use(Vuex);
 
-// Register custom components.
+setOptions({
+    name: 'ht-storybook',
+    url: 'https://github.com/hunterliu1003/ht-storybook',
+  });
+
 Vue.component('my-button', Mybutton);
 
 function loadStories() {
-  // You can require as many stories as you need.
   require('../stories');
 }
 

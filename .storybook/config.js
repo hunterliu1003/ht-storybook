87840d7214a060f5e5d6c92ec38fc70b39d08427
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-15 18:33:43 +0800

    [mod] remove console

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
-import { configure } from '@storybook/vue';
-import { setOptions } from '@storybook/addon-options';
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { configure } from '@storybook/vue'
+import { setOptions } from '@storybook/addon-options'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
@@ -9,15 +9,12 @@ setOptions({
     name: 'ht-storybook',
     url: 'https://github.com/hunterliu1003/ht-storybook',
     addonPanelInRight: true
-  });
+  })
 
 const req = require.context('../components', true, /stories\.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => {
-    console.log(req)
-    return req(filename)
-  });
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module);

008fefda897c189208afc2ad975b3027ea543e41
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-15 16:32:43 +0800

    [mod] myButton add withNodtes

--- stories/index.js
+++ stories/index.js
@@ -2,14 +2,13 @@ import Vue from 'vue';
 
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
-// import { WithNotes } from '@storybook/addon-notes';
+import { withNotes } from '@storybook/addon-notes';
 
 import MyButton from './Button.vue';
 storiesOf('MyButton', module)
-  .add('story as a template', () => ({
+  .add('story as a template', withNotes('myButton with Notes!')(() => ({
     template: `
       <div>
-        <my-button @click.native="clickHandler"></my-button>
         <my-button @click.native="clickHandler"></my-button>
       </div>
     `,
@@ -21,7 +20,7 @@ storiesOf('MyButton', module)
         console.log('asdffdas')
       }
     }
-  }))
+  })))
   .add('story as a component', () => ({
     components: { MyButton },
     template: '<my-button :rounded="true">rounded</my-button>',

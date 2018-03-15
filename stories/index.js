9f4f7ac2c78764d181d264be2fb3e1e4cedec3a4
hunterliu1003@gmail.com <hunterliu1003@gmail.com>
2018-03-15 16:21:18 +0800

    [add] addons actions, links, notes

--- stories/index.js
+++ stories/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue';
 
 import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+// import { WithNotes } from '@storybook/addon-notes';
 
 import MyButton from './Button.vue';
-
 storiesOf('MyButton', module)
   .add('story as a template', () => ({
-    template: '<my-button :rounded="true">story as a function template</my-button>'
+    template: `
+      <div>
+        <my-button @click.native="clickHandler"></my-button>
+        <my-button @click.native="clickHandler"></my-button>
+      </div>
+    `,
+    methods: {
+      clickHandler () {
+        action('MyButton')('click')
+      },
+      haha () {
+        console.log('asdffdas')
+      }
+    }
   }))
   .add('story as a component', () => ({
     components: { MyButton },
-    template: '<my-button :rounded="true">rounded</my-button>'
+    template: '<my-button :rounded="true">rounded</my-button>',
   }));

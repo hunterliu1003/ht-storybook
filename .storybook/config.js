9f2892721fbd1abf8d737b61c2efaa78533b916c
hunterliu1003 <hunterliu1003@gmail.com>
2018-03-22 19:09:18 +0800

    [mod] vue-sortable directive

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,14 @@ import { configure } from '@storybook/vue'
 import { setOptions } from '@storybook/addon-options'
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import Sortable from 'vue-sortable'
 
 Vue.use(Vuex);
+Vue.directive('sortable', {
+  inserted: function (el, binding) {
+    new Sortable(el, binding.value || {})
+  }
+})
 
 require('!!style-loader!css-loader!stylus-loader!../stylus/main.styl');
 

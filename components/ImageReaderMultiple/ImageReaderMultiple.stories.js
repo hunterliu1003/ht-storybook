f9993188483932c198f09dda0f0df031a5a5b6fd
hunterliu1003 <hunterliu1003@gmail.com>
2018-03-22 17:36:34 +0800

    [mod]

--- components/ImageReaderMultiple/ImageReaderMultiple.stories.js
+++ components/ImageReaderMultiple/ImageReaderMultiple.stories.js
@@ -3,6 +3,9 @@ import Vue from 'vue';
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import centered from '@storybook/addon-centered';
+import { withReadme }  from 'storybook-readme';
+import README from './README.md';
+
 
 import ImageReaderMultiple from './';
 
@@ -10,46 +13,46 @@ Vue.component('ImageReaderMultiple', ImageReaderMultiple);
 
 storiesOf('ImageReaderMultiple', module)
   // .addDecorator(centered)
-  .add('ImageReaderMultiple', () => ({
-    data () {
-      return {
-        images: [],
-        images2: []
-      }
-    },
-    watch: {
-      images () {
-        console.log('images')
-        console.log(this.images)
-      }
-    },
-    methods: {
-      log() {
-        action('ImageReaderMultiple')();
+  .add('ImageReaderMultiple', withReadme(README, () => ({
+      data () {
+        return {
+          images: [],
+          images2: []
+        }
       },
-    },
-    template:`
-      <div>
+      watch: {
+        images () {
+          console.log('images')
+          console.log(this.images)
+        }
+      },
+      methods: {
+        log() {
+          action('ImageReaderMultiple')();
+        },
+      },
+      template:`
         <div>
-          imageReaderA
+          <div>
+            imageReaderA
+          </div>
+          <ImageReaderMultiple
+            id="imageReaderA"
+            multiple
+            v-model="images"
+          >
+          </ImageReaderMultiple>
+          <div>
+            imageReaderB
+          </div>
+          <ImageReaderMultiple
+            id="imageReaderB"
+            width="500px"
+            min-height="500px"
+            multiple
+            v-model="images2"
+          >
+          </ImageReaderMultiple>
         </div>
-        <ImageReaderMultiple
-          id="imageReaderA"
-          multiple
-          v-model="images"
-        >
-        </ImageReaderMultiple>
-        <div>
-          imageReaderB
-        </div>
-        <ImageReaderMultiple
-          id="imageReaderB"
-          width="500px"
-          min-height="500px"
-          multiple
-          v-model="images2"
-        >
-        </ImageReaderMultiple>
-      </div>
-    `
-  }));
+      `
+    })));
--- components/ImageReaderMultiple/README.md
+++ components/ImageReaderMultiple/README.md
@@ -1,1 +1,9 @@
 # ImageReaderMultiple Markdown
+
+| props | type | required | default |
+| :---- | :--- | :------: | :------ |
+| id | String | true |  |
+| value | Array | false | [ ] |
+| width | String | false | '100%' |
+| minHeight | String | false | '200px' |
+| multiple | Boolean | false | false |
